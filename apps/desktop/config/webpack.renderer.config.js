1e4ee23ce983686cdbbc116a2bb97c7cbba5239d
Robert Donnelly <dev@robdonn.com>
2020-06-04 16:56:35 +0100

    add desktop

---
+++ apps/desktop/config/webpack.renderer.config.js
@@ -0,0 +1,24 @@
+const rules = require('./webpack.rules');
+
+rules.push({
+  test: /\.css$/,
+  use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+});
+
+rules.push({
+  test: /\.js$/,
+  exclude: /node_modules/,
+  use: {
+    loader: 'babel-loader',
+    options: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
+    },
+  },
+});
+
+module.exports = {
+  // Put your normal webpack config below here
+  module: {
+    rules,
+  },
+};
---
+++ apps/desktop/src/renderer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+const App = () => <h1>Tings</h1>;
+
+ReactDOM.render(<App />, document.querySelector('#app'));
